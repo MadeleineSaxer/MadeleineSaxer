b3a766112d351e0b991966b056d786ffbccb114c
Claudia Saxer <c.s.saxer@gmail.com>
2023-02-21 23:41:50 +0100

    update to angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import {
   MatButtonModule,
   MatCardModule,
@@ -11,24 +11,24 @@ import {
   MatTabsModule,
   MatToolbarModule
 } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GalleryComponent} from './gallery/gallery.component';
-import {ExhibitionComponent} from './exhibition/exhibition.component';
-import {AboutComponent} from './about/about.component';
-import {HttpClientModule} from '@angular/common/http';
-import {NgxGalleryModule} from 'ngx-gallery';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
-import {ContactComponent} from './contact/contact.component';
-import {SoldGalleryComponent} from './soldGallery/soldGallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ExhibitionComponent } from './exhibition/exhibition.component';
+import { AboutComponent } from './about/about.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ContactComponent } from './contact/contact.component';
+import { SoldGalleryComponent } from './soldGallery/soldGallery.component';
 
 const appRoutes: Routes = [
-  {path: 'gallery', component: GalleryComponent},
-  {path: 'gallery-sold', component: SoldGalleryComponent},
-  {path: 'me', component: AboutComponent},
-  {path: 'exhibition', component: ExhibitionComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: '**', component: GalleryComponent}
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'gallery-sold', component: SoldGalleryComponent },
+  { path: 'me', component: AboutComponent },
+  { path: 'exhibition', component: ExhibitionComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: GalleryComponent }
 ];
 
 @NgModule({
